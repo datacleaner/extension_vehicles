632a321eaa81e55d1524230e409d0643a7ca22ce
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2015-10-09 13:39:16 +0200

    Added a TestMain method for easy manual testing

---
+++ src/test/java/org/datacleaner/components/vehicles/TestMain.java
@@ -0,0 +1,12 @@
+package org.datacleaner.components.vehicles;
+
+import org.junit.Ignore;
+
+@Ignore
+public class TestMain {
+
+    // starts DC with this extension in classpath
+    public static void main(String[] args) {
+        org.datacleaner.Main.main(args);
+    }
+}
